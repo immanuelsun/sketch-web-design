3599aa3c86f7716c245e574e9642a4985549d541
Immanuel Sun <immanuelsun@newstreamsdesign.com>
2017-07-17 16:35:10 -0700

    added fontawesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 ——————————————————————————————————— */
 import custom from './js/bs-config';
 
+/* Font Awesome */
+require("font-awesome-loader");
+
 /* Styles
 --------------------------------------------*/
 import styles from './sass/styles.scss';
